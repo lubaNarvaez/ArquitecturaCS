f166ef71b47575c1da393bf10636aeed58a20a53
lubaNarvaez <l.narvaez@utp.edu.co>
2021-09-29 07:00:03 -0500

    Añadiendo balanceo de cargas con sha1

---
+++ BalanceoCargas/balanceo.py
@@ -0,0 +1,58 @@
+import hashlib
+import os
+import uuid
+
+
+listaDec = []
+
+listaSHA = []
+
+rangos = {
+}
+
+for item in range(500):
+    cadena = uuid.uuid4()
+    item = hashlib.new("sha1", cadena.bytes)
+    num = int(item.hexdigest(), 16)
+    listaDec.append(num)
+
+
+listaDec.sort()
+
+for item in listaDec:
+    rangos[item] = []
+
+""" for file in range(2000):
+    namefile = 'archivo-{}.txt'.format(file)
+    with open(namefile, 'wb') as fout:
+        fout.write(os.urandom(1024)) """
+
+
+for file in range(2000):
+    namefile = 'archivo-{}.txt'.format(file)
+    with open(namefile, 'rb') as f: 
+        listaSHA.append(hashlib.sha1(f.read()).hexdigest())
+
+
+
+for item in listaSHA:
+    num = int(item, 16)
+    for dec in listaDec:
+        if num < dec:
+            rangos[dec].append(num)
+            break
+        if num >= listaDec[-1]:
+            rangos[listaDec[0]].append(num)
+            break
+
+
+""" for item in listaDec:
+    print(item)
+print('')
+for item in listaSHA:
+    print(int(item, 16))
+print('') """
+for item in rangos.keys():
+    print(item)
+    print(len(rangos.get(item)))
+    print('')
---
+++ Entrega1/Server/servidor.py
@@ -0,0 +1,79 @@
+import zmq
+import os
+import hashlib
+
+
+# BUF_SIZE is totally arbitrary, change for your app!
+BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
+SIZE = 1048576
+users = {}
+context = zmq.Context()
+s = context.socket(zmq.REP)
+s.bind('tcp://*:8001')
+
+while True:
+    op= s.recv_string()
+    s.send_string('ok')
+    if(op=='upload'):
+        user = s.recv_string()
+        s.send_string('ok')
+        file = s.recv_string()
+        s.send_string('ok')
+        with open(file, 'ab') as f:
+            Mb = s.recv_multipart()
+            f.write(Mb[0])
+            s.send_string('ok')
+
+    elif(op  == 'download'):
+        user = s.recv_string()
+        s.send_string('ok')
+        file = s.recv_string()
+        contador =open (file, 'rb')
+        if users.get(user)==None:
+            users[user] = 0
+        pos = users[user]
+        contador.seek(pos)
+        byte = contador.read(SIZE)
+        users[user]= contador.tell()
+        s.send_multipart([byte])
+
+    elif(op  == 'list'):
+        user = s.recv_string()
+        arc = os.listdir('.')
+        cont = '\n'.join(arc)    
+        s.send_string(cont)
+
+    elif(op  == 'sharelink'):
+        user = s.recv_string()
+        s.send_string('ok')
+        file = s.recv_string()
+        md5 = hashlib.md5()
+        with open (file, 'rb') as f:
+            while True:
+                data = f.read(BUF_SIZE)
+                if not data:
+                    break
+                md5.update(data)
+            link = md5.hexdigest()
+            s.send_string(link)
+
+    elif(op  == 'downloadlink'):
+        user = s.recv_string()
+        s.send_string('ok')
+        link = s.recv_string()
+        arcs = os.listdir('.')
+        for arc in arcs:
+            md5 = hashlib.md5()
+            with open (arc, 'rb') as f:
+                while True:
+                    data = f.read(BUF_SIZE)
+                    if not data:
+                        break
+                    md5.update(data)
+            if link == md5.hexdigest():
+                s.send_string(arc)
+                break
+                
+                
+    
+    
